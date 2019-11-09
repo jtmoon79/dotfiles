c7149948bb2936bf617a3cc825345c7416d4162a
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2019-11-08 21:35:55 -0800

    Move download code to a function, only download relevant files

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
 # install other . (dot) files in a one-liner, for fast setup of a new Unix user shell environment
+# parameters passed to this script will be passed as options to wget or curl
 #
 # run this file:
 #
@@ -12,19 +13,33 @@
 set -e
 set -u
 
-if which wget &>/dev/null; then
-    set -x
-    wget "${@}" -O ./.vimrc 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.vimrc'
-    wget "${@}" -O ./.bash_profile 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.bash_profile'
-    wget "${@}" -O ./.bashrc 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.bashrc'
-    wget "${@}" -O ./.screenrc 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.screenrc'
-elif which curl &>/dev/null; then
-    set -x
-    curl "${@}" --output ./.vimrc 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.vimrc'
-    curl "${@}" --output ./.bash_profile 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.bash_profile'
-    curl "${@}" --output ./.bashrc 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.bashrc'
-    curl "${@}" --output ./.screenrc 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.screenrc'
-else
-    echo 'ERROR: cannot find either program wget or curl' >&2
-    exit 1
+function download () {
+    declare to_=${1}
+    shift
+    declare from_=${1}
+    shift
+    if which wget &>/dev/null; then
+        (
+            set -x
+            wget "${@}" -O "${to_}" "${from_}"
+        )
+    elif which curl &>/dev/null; then
+        (
+            set -x
+            curl "${@}" --output "${to_}" "${from_}"
+        )
+    else
+        echo 'ERROR: cannot find either program wget or curl' >&2
+        exit 1
+    fi
+}
+
+download './.bash_profile' 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.bash_profile' "${@}"
+download './.bashrc' 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.bashrc' "${@}"
+download './.bash_logout' 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.bash_logout' "${@}"
+if which screen &>/dev/null; then
+    download './.screenrc' 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.screenrc' "${@}"
+fi
+if which vim &>/dev/null; then
+    download './.vimrc' 'https://raw.githubusercontent.com/jtmoon79/dotfiles/master/.vimrc' "${@}"
 fi
