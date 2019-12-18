843aa174ced809c362a0ffa371714ea306da59bd
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2019-12-18 14:21:40 -0800

    Add docker-test.sh, rename docker-tests.sh

---
+++ docker-test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# load a bash instance with .bash profiles
+
+set -e
+set -u
+
+cd "$(dirname -- "${0}")"
+
+# images at https://hub.docker.com/_/bash
+IMAGE=${1-'bash:4.0'}
+DTNOW=$(date '+%Y%m%dT%H%M%S')
+HOSTNAME=${IMAGE/./-}-${DTNOW}
+HOSTNAME=${HOSTNAME/:/-}
+
+(
+    set -x
+    docker run \
+        -e 'color_force=true' \
+        -e 'prompt_bullet=→' \
+        -v "${PWD}:/root" \
+        --hostname "${HOSTNAME}" \
+        --name "${HOSTNAME}" \
+        --rm \
+        -it "${IMAGE}" \
+        bash --login
+)
