e32cd2b52dcc7ad9896da3502ae53896eadcc5eb
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2020-10-01 13:14:16 -0700

    archive-encrypt do not reprint password

--- utilities/archive-encrypt.sh
+++ utilities/archive-encrypt.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #
-# quick imperfect script to tar a path and 7zip encrypt the tar
-# useful for quick encrypted backups
+# Quick imperfect script to tar a path and 7zip encrypt the tar.
+# Useful for quick encrypted backups.
 
 set -e
 set -u
 set -o pipefail
 
-BACKUP_DIR=${BACKUP_DIR-'/root/backups'}
+BACKUP_DIR=${BACKUP_DIR-${HOME}/backups}
 
 function backup_name_tar() {
     echo -n "${BACKUP_DIR}/$(hostname)__${1}__$(date +%Y%m%d).tar"
@@ -18,7 +18,7 @@ if [[ ${#} != 1 && ${#} != 2 ]] || [[ "${1:-}" == '--help' ]]; then
     echo "usage:
     ${0} /path/to/backup [/backup/destination/dir]
 
-Tar and 7zip a target path.
+Tar and 7zip a target directory path.
 Tar to preserve filesystem permissions and layout. 7zip to compress and encrypt.
 REQUIRES ENTERING A PASSWORD.
 
@@ -39,7 +39,7 @@ if [[ ! -e "${target}" ]]; then
     echo "ERROR: Target '${target}' does not exist" >&2
     exit 1
 fi
-BACKUP_DIR=${2:-${BACKUP_DIR}}
+declare -r BACKUP_DIR=${2:-${BACKUP_DIR}}
 readonly BACKUP_DIR target
 
 name=$(basename -- "$(readlink -f -- "${target}")")
@@ -48,20 +48,16 @@ archive_tar7z="${archive_tar}.7z"
 readonly name archive_tar archive_tar7z
 
 read -p "Enter the 7z archive password: " -s password
-#password_file=$(mktemp)
-#echo -n "${password}" > "${password_file}"
-#function exit_() {
-#    rm -f "${password_file}" &>/dev/null || true
-#}
-#trap exit_ EXIT
+echo
 
 (
-set -x
-
 function exit_() {
     rm -vf -- "${archive_tar}"
 }
 trap exit_ EXIT
+
+(
+set -x
 
 tar \
   --create \
@@ -76,20 +72,26 @@ tar \
 tar \
   -t \
   -f "${archive_tar}" >/dev/null
+)
 
-# stupidly, echoes the password :-(
+declare -a s7z_args=(
+  '-t7z'
+  '-m0=lzma2'
+  '-mx=9'
+  '-mfb=64'
+  '-md=32m'
+  '-ms=on'
+  '-mhe=on'
+  "${archive_tar7z}"
+  "${archive_tar}"
+)
+
+echo "${PS4-}7z a -p****" "${s7z_args[@]}" >&2
 7z a \
   -p"${password}" \
-  -t7z \
-  -m0=lzma2 \
-  -mx=9 \
-  -mfb=64 \
-  -md=32m \
-  -ms=on \
-  -mhe=on \
-   "${archive_tar7z}" "${archive_tar}" \
+  "${s7z_args[@]}"
 
-# echoes the password :-(
+echo "${PS4-}7z l -p****" -slt "${archive_tar7z}" >&2
 7z l \
    -p"${password}" \
    -slt \
@@ -100,8 +102,6 @@ echo "Success! Restore this archive with
 
     7z e '${archive_tar7z}' -so | tar --verbose -xf - -C /some/path
 
-The blank prompt is expecting the password: ${password}
-
-Consider closing this shell view to remove passwords from scrollback.
-"
+The blank prompt is expecting the password.
+" >&2
 
