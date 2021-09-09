8b3272048c831a96b406e2790f7aa6b860db9f4f
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2021-09-08 19:59:11 -0700

    docker-test.sh add -h

--- docker-test.sh
+++ docker-test.sh
@@ -7,7 +7,22 @@ set -u
 
 cd "$(dirname -- "${0}")"
 
-# images at https://hub.docker.com/_/bash
+if [[ "${1-}" = '-h' || "${1-}" = '--help' || "${1-}" = '-?' ]]; then
+    echo "usage:
+    $(basename -- "${0}") [bash-image]
+
+    where bash-image is a docker hub bash image, e.g. 'bash:4.0' or 'bash:latest' or 'bash'
+
+about:
+
+    For testing the dot files with different versions of bash.
+    Run a bash login shell using bash dot files within a docker container.
+
+    Public docker hub bash images are at https://hub.docker.com/_/bash
+" >&2
+    exit 1
+fi
+
 IMAGE=${1-'bash:4.0'}
 DTNOW=$(date '+%Y%m%dT%H%M%S')
 HOSTNAME=${IMAGE/./-}-${DTNOW}
