cfe2483dfb5669147cbaf706d999e97326c32dd5
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-12-22 15:20:52 -0800

    ping-beat.sh add percentage dots, netcat option

--- utilities/ping-beat.sh
+++ utilities/ping-beat.sh
@@ -16,34 +16,49 @@ All arguments are passed to ping.
 
 About:
 
-A script to visually monitor a network connection over a long period of time
-using ICMP ping in a per-minute visualization.
+A script to visually monitor a network connection over a long duration of time
+in a per-minute visualization. It uses ICMP to deteremine if a host is
+online.
 
-Strongly recommended to pass HOSTNAME in numeric form.
+It is strongly recommended to pass HOSTNAME in numeric address form.
 
 Output looks like:
 
-    2022-12-20T21:28:00 ·························································***
-    2022-12-20T21:29:00 ***·························································
-    2022-12-20T21:30:00 ····························································
-    2022-12-20T21:31:00 ····························································
+    2022-12-20T15:45:00 ▃▃▃▃▃▃▃▃▄▃▃▃▅▃▃▃▃▃▃▃▃▃▃▅▃▃▃▃▃▃█▃▃█▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▇▃▃▃▃▃▃▃╳╳
+    2022-12-20T15:46:00 ╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳
+
+(hopefully the characters are aligned)
 
 Any user input will be used to demarcate a point in time.
 For example, if the user presses key 'D' then output will look like:
 
-    2022-12-20T21:32:00 ************************************************************
-    2022-12-20T21:32:00 ***D**······················································
+    2022-12-20T15:46:00 ╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳D╳╳╳╳▃▄▃▃▃▃▃
+
 
 In the contrived example, 'D' is an arbitrary user mnemonic, say Disconnecting
-some network equipment. Three seconds after that event the pings began to succeed.
+some network equipment. Three seconds after that event the pings began to
+succeed.
 
-Requires ping from package iputils, and GNU date.
+Setting environment variable 'PING_BEAT_NOCOLOR' to any value will turn off
+color escape sequences.
 
-Setting environment variable 'PING_BEAT_COLOR' to any value will colorize the
-output.
+Setting environment variable 'PING_BEAT_TIMEONLY' to any value will print
+only the Hour, Minute, and Second.
 
 Setting environment variable 'PING_BEAT_LOG' to a file path will append the
 non-colorized output to that file.
+
+Setting environment variable 'PING_BEAT_ASCII' to any value will print only
+ASCII characters.
+This is useful for visual alignment problems that may occur with some font
+and terminal combinations.
+
+Setting environment variable 'PING_BEAT_CONNECT_TCP' to any value will use
+netcat to create a TCP connection to port. Port is the second script argument.
+
+Requires programs ping from package iputils or busybox, and date from GNU
+coreutils.
+
 " >&2
     exit 1
 fi
@@ -61,7 +76,7 @@ if ! dateout=$(date '+%N' 2>&1); then
 fi
 unset dateout
 date1=$(date '+%N' 2>&1)
-sleep 0.1
+sleep 0.01
 date2=$(date '+%N' 2>&1)
 if [[ "${date1}" = "${date2}" ]] &>/dev/null; then
     echo "ERROR date failed to process '+%s%N'" >&2
@@ -70,8 +85,13 @@ fi
 unset date1 date2
 
 function ping_cmd() {
-    # designed for `ping` from package `iputils`
-    ping -c1 -n -q -w1 "${@}"
+    if ! ${do_netcat}; then
+        # designed for `ping` from package `iputils` or busybox
+        ping -c1 -n -q -w1 "${@}"
+    else
+        # tested against BSD `nc` from package
+        nc -N -d -w 1 "${@}"
+    fi
 }
 
 function parse_rtt_avg() {
@@ -80,7 +100,7 @@ function parse_rtt_avg() {
     #    fractional) and return 0
     # else return 1
     #
-    # reference `ping` output:
+    # reference output from `iputils` ping:
     #
     #   $ ping -c2 1.1.1.1
     #   PING 1.1.1.1 (1.1.1.1) 56(84) bytes of data.
@@ -91,8 +111,24 @@ function parse_rtt_avg() {
     #   2 packets transmitted, 2 received, 0% packet loss, time 1001ms
     #   rtt min/avg/max/mdev = 11.944/17.223/22.502/5.279 ms
     #
+    # TODO: what does ping time greater than 1 second look like?
+    #       do milliseconds become just seconds?
+    #
+    # reference output from busybox ping:
+    #
+    #     $ ping -c1 -n -q -w1 127.0.0.1
+    #     PING 127.0.0.1 (127.0.0.1): 56 data bytes
+    #
+    #     --- 127.0.0.1 ping statistics ---
+    #     1 packets transmitted, 1 packets received, 0% packet loss
+    #     round-trip min/avg/max = 0.121/0.121/0.121 ms
+    #
     declare avg=
-    if ! avg=$(echo -n "${1}" | tail -n 3 | grep -Ee '^rtt ' | cut -f 6 -d '/') 2>/dev/null; then
+    if avg=$(echo -n "${1}" | tail -n 3 | grep -Ee '^rtt ' | cut -f 6 -d '/') 2>/dev/null; then
+        true
+    elif avg=$(echo -n "${1}" | tail -n 3 | grep -Ee '^round-trip ' | cut -f 4 -d '/') 2>/dev/null; then
+        true
+    else
         return 1
     fi
     # $avg should be a string like "17.223"
@@ -106,39 +142,51 @@ function parse_rtt_avg() {
     echo -n "${avgi}"
 }
 
+
+# optional environment flag
+do_netcat=false
+if [[ ! -z "${PING_BEAT_CONNECT_TCP-}" ]]; then
+    do_netcat=true
+fi
+readonly do_netcat
+
 # check that `ping` works as expected
 # there are several varying `ping` commands among Unixii
-# so good to check othewise user might think all pings are failing.
-if ! out=$((set -x; ping_cmd 127.0.0.1) 2>&1); then
-    echo "ERROR $(which ping) failed for 127.0.0.1; is it ping from package BSD iputils?" >&2
+# so make sure this one is acceptable othewise user might think all pings are
+# failing.
+if ! ${do_netcat} && ! out=$((set -x; ping_cmd 127.0.0.1) 2>&1); then
+    echo "ERROR $(which ping) failed for 127.0.0.1; is it ping from package iputils or busybox?" >&2
     echo >&2
     echo "${out}" >&2
     exit 1
 fi
 unset out
 
-# suppress echoing user input, it'll mess up the line-by-line visual
+# suppress echoing user input, restore tty settings upon exit
 # from https://stackoverflow.com/a/4316765/471376
-stty_orig=$(stty -g)
-trap "stty ${stty_orig}" EXIT
+stty_conf=$(stty -g)
+trap "stty ${stty_conf}" EXIT
 stty -echo
+readonly stty_conf
+
+# number of sub-second fractional nanoseconds, e.g. nanoseconds in one second
+declare -ir n_in_s=1000000000
+# number of sub-minute fractional nanoseconds, e.g. nanoseconds in one minute
+declare -ir n_in_m=$((n_in_s * 60))
 
 # current time in Unix epoch seconds but round down to minute
-function time_now_round_minute() {
+function datetime_now_round_minute() {
     echo -n $((($(date '+%s') / 60) * 60))
 }
 
-function print_datetime()  {
-    date '+%Y-%m-%dT%H:%M:%S' | tr -d '\n'
+# current datetime in seconds&nanoseconds
+function datetime_now_sn() {
+    date '+%s%N'
 }
 
-#
-# printing leading part of the first line; special case
-#
-
 declare -i now_s=$(date '+%s')
-declare -i minute_a=$(((${now_s} / 60) * 60))
-declare -i seconds_in_min=$((${now_s} % 60))
+declare -i now_minute_a_s=$(((${now_s} / 60) * 60))
+declare -i seconds_into_min=$((${now_s} % 60))
 unset now_s
 
 # prepare optional PING_BEAT_LOG
@@ -150,19 +198,47 @@ if [[ ! -z "${PING_BEAT_LOG-}" ]]; then
     if [[ -s "${PING_BEAT_LOG}" ]]; then
         echo >> "${PING_BEAT_LOG}"
     fi
-    echo "${PS4}ping ${@}" >> "${PING_BEAT_LOG}"
+    echo "${PS4}$(basename -- "${0}") ${@}" >> "${PING_BEAT_LOG}"
 else
     PING_BEAT_LOG=/dev/null
 fi
 readonly PING_BEAT_LOG
 
+# optional environment flag
+do_only_ascii=false
+if [[ ! -z "${PING_BEAT_ASCII-}" ]]; then
+    do_only_ascii=true
+fi
+readonly do_only_ascii
+
+# optional enviroment flag
+do_datetime_timeonly=false
+if [[ ! -z "${PING_BEAT_TIMEONLY-}" ]]; then
+    do_datetime_timeonly=true
+fi
+readonly do_datetime_timeonly
+
+function print_datetime()  {
+    declare format="+%Y-%m-%dT%H:%M:${1-%S}"
+    if ${do_datetime_timeonly}; then
+        format="+%H:%M:${1-%S}"
+    fi
+    date "${format}" | tr -d '\n'
+}
+
+#
+# first script printing
+#
+
 # print current HMS
 echo -n "$(print_datetime) " | tee -a "${PING_BEAT_LOG}"
 # fillup used seconds with spaces
-if [[ ${seconds_in_min} -gt 0 ]]; then
-    printf -- ' %.0s' $(seq 1 ${seconds_in_min}) | tee -a "${PING_BEAT_LOG}"
+declare -i minute_dot_at=0
+if [[ ${seconds_into_min} -gt 0 ]]; then
+    printf -- ' %.0s' $(seq 1 ${seconds_into_min}) | tee -a "${PING_BEAT_LOG}"
+    minute_dot_at=${seconds_into_min}
 fi
-unset seconds_in_min
+unset seconds_into_min
 
 #
 # print remaining dots for the first line and all proceeding lines
@@ -174,72 +250,84 @@ unset seconds_in_min
 # Use box-drawing to communicate RTT times.
 # See https://en.wikipedia.org/wiki/Box-drawing_character
 #
-declare -r good='·' # '·' 'ꞏ'
-declare -r good08=' ' # 0/8
-declare -r good18='▁' # 1/8
-declare -r good28='▂' # 2/8
-declare -r good38='▃' # 3/8
-declare -r good48='▄' # 4/8
-declare -r good58='▅' # 5/8
-declare -r good68='▆' # 6/8
-declare -r good78='▇' # 7/8
-declare -r good88='█' # 8/8
-declare -r bad='*'  # '¤' '*' 'X'
-declare -r bad99='╳'
+
+# ping result dots
+declare -r dot_good_ASCII='·' # ASCII Extended
+declare -r dot_good='·' # fallback
+declare -r dot_good08=' ' # 0/8
+declare -r dot_good18='▁' # 1/8
+declare -r dot_good28='▂' # 2/8
+declare -r dot_good38='▃' # 3/8
+declare -r dot_good48='▄' # 4/8
+declare -r dot_good58='▅' # 5/8
+declare -r dot_good68='▆' # 6/8
+declare -r dot_good78='▇' # 7/8
+declare -r dot_good88='█' # 8/8
+declare -r dot_good99='░' # >8/8
+declare -r dot_bad_ASCII='*' # ASCII
+declare -r dot_bad99='╳'
+declare -r dot_skip=' '
+declare -r dot_skip_ASCII=' '
+# ping result colors
 declare -r color_good='\e[32m' # red
 declare -r color_bad='\e[31m'  # green
 declare -r color_norm='\e[0m'  # normalize
 # set optional colorization
 # using a bool is a little easier to grok instead of string check
-do_color=false
-if [[ ! -z "${PING_BEAT_COLOR-}" ]]; then
-    do_color=true
+do_color=true
+if [[ ! -z "${PING_BEAT_NOCOLOR-}" ]]; then
+    do_color=false
 fi
 readonly do_color
+
 user_input=
 unset ping_out
 
 while true; do
     # save the exact time before long-running `ping` command
-    declare -i start_sn=$(date '+%s%N')
-    # do the `ping` command, print results
+    declare -i start_sn=$(datetime_now_sn)  # loop start datetime in seconds&nanoseconds
+    # do the `ping` command, print results based on various flags and inputs
     if ping_out=$(ping_cmd "${@}" 2>&1); then
         if ${do_color}; then
             echo -en "${color_good}"
         fi
         declare -i avg=0
         if [[ ! -z "${user_input}" ]]; then
-            out="${user_input:0:1}"
+            dot="${user_input:0:1}"
+        elif ${do_only_ascii}; then
+            dot="${dot_good_ASCII}"
         elif avg=$(parse_rtt_avg "${ping_out}" 2>/dev/null); then
             # `ping` waits one second for replies
             # print a visual indicator of RTT time for the scale 0 to 100
             # milliseconds
             if [[ ${avg} -le 0 ]]; then
-                out="${good08}"
-            elif [[ ${avg} -le 13 ]]; then
-                out="${good18}"
-            elif [[ ${avg} -le 25 ]]; then
-                out="${good28}"
-            elif [[ ${avg} -le 38 ]]; then
-                out="${good38}"
-            elif [[ ${avg} -le 50 ]]; then
-                out="${good48}"
-            elif [[ ${avg} -le 63 ]]; then
-                out="${good58}"
-            elif [[ ${avg} -le 75 ]]; then
-                out="${good68}"
-            elif [[ ${avg} -le 88 ]]; then
-                out="${good78}"
-            elif [[ ${avg} -le 100 ]]; then
-                out="${good88}"
+                dot="${dot_good08}"
+            elif [[ ${avg} -le 130 ]]; then
+                dot="${dot_good18}"
+            elif [[ ${avg} -le 250 ]]; then
+                dot="${dot_good28}"
+            elif [[ ${avg} -le 380 ]]; then
+                dot="${dot_good38}"
+            elif [[ ${avg} -le 500 ]]; then
+                dot="${dot_good48}"
+            elif [[ ${avg} -le 630 ]]; then
+                dot="${dot_good58}"
+            elif [[ ${avg} -le 750 ]]; then
+                dot="${dot_good68}"
+            elif [[ ${avg} -le 880 ]]; then
+                dot="${dot_good78}"
+            elif [[ ${avg} -le 1000 ]]; then
+                dot="${dot_good88}"
             else
-                out="${good88}"
+                # this shouldn't happen; ping timeout is 1 second and
+                # `ping` prints in milliseconds
+                dot="${dot_good99}"
             fi
         else
             # the ping succeeded but parsing RTT failed
-            out="${good}"
+            dot="${dot_good}"
         fi
-        echo -n "${out}" | tee -a "${PING_BEAT_LOG}"
+        echo -n "${dot}" | tee -a "${PING_BEAT_LOG}"
         if ${do_color}; then
             echo -en "${color_norm}"
         fi
@@ -248,34 +336,101 @@ while true; do
             echo -en "${color_bad}"
         fi
         if [[ ! -z "${user_input}" ]]; then
-            out="${user_input:0:1}"
+            dot="${user_input:0:1}"
+        elif ${do_only_ascii}; then
+            dot="${dot_bad_ASCII}"
         else
-            out="${bad99}"
+            dot="${dot_bad99}"
         fi
-        echo -n "${out}" | tee -a "${PING_BEAT_LOG}"
+        echo -n "${dot}" | tee -a "${PING_BEAT_LOG}"
         if ${do_color}; then
             echo -en "${color_norm}"
         fi
     fi
+    #
     # print datetime only at the minute rollovers
-    declare -i minute_b=$(time_now_round_minute)
-    if [[ ${minute_a} -ne ${minute_b} ]]; then
-        minute_a=${minute_b}
-        echo -en "\n$(print_datetime) " | tee -a "${PING_BEAT_LOG}"
+    #
+    # current datetime in seconds&nanoseconds
+    declare -i now_sn=$(datetime_now_sn)
+    # current datetime in seconds&nanoseconds truncated minute
+    declare -i now_minute_sn=$(((now_sn / n_in_m) * n_in_m))
+    # next minute datetime in seconds&nanoseconds truncated minute
+    declare -i now_minute_b_sn=$(($now_minute_sn + n_in_m))
+    # current datetime in seconds truncated minute
+    declare -i now_minute_b_s=$((((now_sn / n_in_s) / 60) * 60))
+    if [[ ${now_minute_a_s} -ne ${now_minute_b_s} ]]; then
+        # reached the minute rollover datetime
+        while [[ ${minute_dot_at} -lt 59 ]]; do
+            # reached the end of the minute time, but there are still dots that
+            # have not been printed. So quickly print remaining "skip" dots.
+            # This should result in 60 dots printed per row.
+            if ${do_only_ascii}; then
+                dot="${dot_skip_ASCII}"
+            else
+                dot="${dot_skip}"
+            fi
+            echo -n "${dot}" | tee -a "${PING_BEAT_LOG}"
+            minute_dot_at+=1
+        done
+        now_minute_a_s=${now_minute_b_s}
+        # print the start of a new row
+        echo -en "\n$(print_datetime 00) " | tee -a "${PING_BEAT_LOG}"
+        minute_dot_at=0
+        declare -i remain_s=60
+    else
+        minute_dot_at+=1
+        # remaining nanoseconds in current minute
+        declare -i remain_ns=$((n_in_m - (now_sn % now_minute_sn)))
+        if [[ ${remain_ns} -lt 0 ]]; then
+            remain_ns=0
+        fi
+        # remaining seconds in cur_minute
+        declare -i remain_s=$((remain_ns / n_in_s))
     fi
-    # sleep the remaining amount of subsecond nanoseconds
-    declare -i next_second=$(((${start_sn} / 1000000000) * 1000000000 + 1000000000))
-    declare -i wait_ns=$((${next_second} - $(date '+%s%N')))
-    if [[ ${wait_ns} -gt 0 ]]; then
-        declare -i sleep_ns=$((${wait_ns} % 1000000000))
-        sleep "0.${sleep_ns}"
+    #
+    # sleep up for duration that this should be at for the row minute
+    #
+    declare -i minute_dot_expect=$((60 - remain_s))
+    # wait duration in nanoseconds (not a sub-second fractional)
+    declare -i wait_n=0
+    if [[ ${minute_dot_expect} -le ${minute_dot_at} ]]; then
+        # getting ahead of things or nearly on-track (within .1 second)
+        #
+        # add sub-second nanoseconds to wait minus .1 second
+        # (allow starting the next loop iteration a little bit early)
+        wait_n=$(((minute_dot_at - minute_dot_expect) * n_in_s - 100000000))
+        # add seconds to wait but as nanoseconds
+        wait_n+=$((now_sn % n_in_s))
+        if [[ ${wait_n} -gt 0 ]]; then
+            declare -i sleep_n=$((${wait_n} % n_in_s))
+            declare sleep_n_str=$(printf '%09d' ${sleep_n})
+            declare -i sleep_s=$((${wait_n} / n_in_s))
+            sleep "${sleep_s}.${sleep_n}"
+        fi
+        #minute_dot_at+=1
+    else
+        # falling behind too far behind,
+        # skip the next ping(s), print skip dot for this "second column"
+        declare -i skip_ahead=$((${minute_dot_expect} - ${minute_dot_at}))
+        while [[ ${skip_ahead} -gt 0 ]]; do
+            if ${do_only_ascii}; then
+                dot="${dot_skip_ASCII}"
+            else
+                dot="${dot_skip}"
+            fi
+            echo -n "${dot}" | tee -a "${PING_BEAT_LOG}"
+            minute_dot_at+=1
+            skip_ahead=$((skip_ahead - 1))
+        done
     fi
-    # read user input
+    #
+    # read user input for optional overwrite
+    #
     # XXX: read does not accept wait time value zero (`-t0`) so use a very small
     #      wait time
     user_input=
     IFS= read -s -t0.0001 -n1 user_input &>/dev/null || true
-    # flush the remaining user input
-    IFS= read -s -t0.0001 THROWAWAY &>/dev/null || true
+    # flush the remaining user input so accidental keysmash does not print
+    # a bunch of non-sense over a long duration
+    #IFS= read -s -t0.0001 THROWAWAY &>/dev/null || true
 done
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
