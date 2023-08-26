e41ba0d304dcc0048eba8f9e9afe5f7e916e28d0
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2023-08-26 14:43:30 -0700

    PowerShell_profile.ps1 add functions, catch errors

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,77 @@
-# Custom Windows Powershell
+# Microsoft_PowerShell_profile.ps1
 #
-# startup writes
+# custom Windows Powershell profile
 #
-#    Powershell 5.1.18362.145
-#
-# adds custom aliases
-#
-# copy this file to $env:HOMEPATH\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+# copy this file to ${env:HOMEPATH}\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 # this file is available at https://github.com/jtmoon79/dotfiles/blob/master/Microsoft.PowerShell_profile.ps1
+#
 
-# writes once on startup
-Write-Host "Powershell" $PSVersionTable.PSVersion "`n"
+if ( (Get-Variable -Name PSCommandPath -Scope Global -ErrorAction SilentlyContinue) -and ($PSCommandPath -ne $null) -and ($PSCommandPath -ne "") ) {
+    Write-Host "$PSCommandPath" -ForegroundColor Yellow
+}
+Write-Host "$(Get-Process -Id $PID | Select-Object -ExpandProperty path) " -Nonewline -ForegroundColor Magenta
+Write-Host $PSVersionTable.PSVersion -ForegroundColor Magenta
+
+# turn off check; the check can take tens of seconds on a slow network
+$env:POWERSHELL_UPDATECHECK = 'Off'
+
+# force UTF-8
+# from https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.2
+# https://archive.ph/S8uhz
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+
+#
+# custom functions
+#
+
+function Print-Env {
+    Get-ChildItem env:* | Sort-Object Name
+}
+
+function Print-Path
+{
+    $env:Path -replace ";","`n"
+
+    reg query "HKEY_CURRENT_USER\Environment" /f Path /t REG_EXPAND_SZ
+    reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f Path /t REG_EXPAND_SZ
+}
+
+# handy functions "Where am I?"
+# ripped from https://stackoverflow.com/a/43643346
+ function PSCommandPath() {
+    return $PSCommandPath
+}
+function ScriptName() {
+    return $MyInvocation.ScriptName
+}
+function MyCommandName() {
+    return $MyInvocation.MyCommand.Name
+}
+function MyCommandDefinition() {
+    # Begin of MyCommandDefinition()
+    # Note: output of this script shows the contents of this function, not the execution result
+    return $MyInvocation.MyCommand.Definition
+    # End of MyCommandDefinition()
+}
+function MyInvocationPSCommandPath() {
+    return $MyInvocation.PSCommandPath
+}
+
+# copied from https://devblogs.microsoft.com/powershell/format-xml/
+# use it like:
+#      Format-XML ([xml](cat C:\path\to\file.xml)) -indent 4
+function Format-XML ([xml]$xml, $indent=2)
+{
+    $StringWriter = New-Object System.IO.StringWriter
+    $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter
+    $xmlWriter.Formatting = "indented"
+    $xmlWriter.Indentation = $Indent
+    $xml.WriteContentTo($XmlWriter)
+    $XmlWriter.Flush()
+    $StringWriter.Flush()
+    Write-Output $StringWriter.ToString()
+}
 
 #
 # custom aliases
@@ -19,23 +80,17 @@ Write-Host "Powershell" $PSVersionTable.PSVersion "`n"
 try
 {
     # two ways to create an alias, New-Alias and New-Item
-    # XXX: set the "Source" property to this script (seen in the `alias` command)
     New-Alias -Name "which" -Description "just like Unix!" -Value Get-Command -Option Constant -ErrorAction SilentlyContinue
+    # XXX: how to set Source property to "Microsoft.PowerShell_profile.ps1" ? This can be seen as a column in the `alias` command
     New-Item -path alias:np -value c:\windows\notepad.exe -ErrorAction SilentlyContinue | Out-Null
-} catch {
-    # pass
-}
+} catch {}
 
-function Print-Env {
-	Get-ChildItem env:* | Sort-Object Name
-}
 New-Alias -Name "env" -Description "sorta' like Unix!" -Value Print-Env -ErrorAction SilentlyContinue
 
 #
 # prompt improvement
 #
 
-# copied from https://stackoverflow.com/a/46991468/471376
 function prompt {
     Write-Host "[$Env:username@$Env:computername] " -NoNewline
     Write-Host "$($PWD.ProviderPath) " -ForegroundColor Cyan -NoNewline
@@ -43,10 +98,23 @@ function prompt {
     return " "  # must return something or else powershell will automatically tack on "PS>"
 }
 
-# run Linux Substystem "vim" program like it's a normal Windows program
+#
 # something like an alias but really a shortcut to Windows Linux Subsystem vim application
+#
+
+# run Linux "vim" like it's a "normal" Windows program
 function vim ($File){
-    # XXX: need to allow variable length argument list so "vim" can be passed options
-    $File = $File -replace '\\', '/'
-    bash -c "vim -- '$File'"
+    $File = $File -replace "\\", "/"
+    bash -c vim -- '$File'
+}
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Write-Host "Import-Module `"${ChocolateyProfile}`"" -ForegroundColor Yellow
+    Import-Module "$ChocolateyProfile"
 }
