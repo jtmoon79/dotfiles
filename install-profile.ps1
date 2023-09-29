0ee0acc83757590fc0b71f80d83c847affe9d637
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2023-09-28 22:32:33 -0700

    install-profile.ps1 fix URL to run

--- install-profile.ps1
+++ install-profile.ps1
@@ -4,7 +4,7 @@
 #
 # Run this:
 #
-#    $(Invoke-WebRequest -Uri "https://gist.githubusercontent.com/jtmoon79/ea855a1797e012921c634339ecb4fc40/raw/22735dc95bc28cfbadab2d568d42b8491cba4cfb/install-profile.ps1").Content | powershell -NoLogo -NoProfile -NonInteractive -Command -
+#    $(Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jtmoon79/dotfiles/master/install-profile.ps1").Content | powershell -NoLogo -NoProfile -NonInteractive -Command -
 
 Invoke-WebRequest `
     -Uri "https://raw.githubusercontent.com/jtmoon79/dotfiles/master/Microsoft.PowerShell_profile.ps1" `
