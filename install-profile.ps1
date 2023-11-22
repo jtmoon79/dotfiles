c456909cec6b914e683d5d843464ccffcd093803
James Thomas Moon <815261+jtmoon79@users.noreply.github.com>
2023-11-21 18:37:51 -0800

    install-profile.ps1 mkdir if it does not exist

--- install-profile.ps1
+++ install-profile.ps1
@@ -6,6 +6,11 @@
 #
 #    $(Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jtmoon79/dotfiles/master/install-profile.ps1").Content | powershell -NoLogo -NoProfile -NonInteractive -Command -
 
+$ProfileInfo = [System.IO.FileInfo]$PROFILE
+if (-not (Test-Path $ProfileInfo.DirectoryName)) {
+    New-Item -Type Directory -Path $ProfileInfo.DirectoryName
+}
+
 Invoke-WebRequest `
     -Uri "https://raw.githubusercontent.com/jtmoon79/dotfiles/master/Microsoft.PowerShell_profile.ps1" `
     -OutFile $PROFILE `
