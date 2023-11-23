0c3b0190791ae6e573afe406490bcb677b3789b1
James Thomas Moon (@jtmoon79) <815261+jtmoon79@users.noreply.github.com>
2023-11-22 22:57:01 -0800

    Powershell profile add Print-ProcessTree

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -162,6 +162,36 @@ function global:Format-XML ($xml_file, $indent=2)
 }
 Write-Host "defined Format-XML(xml_file, indent=2)" -ForegroundColor DarkGreen
 
+function Print-ProcessTree() {
+
+    function Get-ProcessAndChildProcesses($Level, $Process) {
+        "{0}[{1,-5}] [{2}]" -f ("  " * $Level), $Process.ProcessId, $Process.Name
+        $Children = $AllProcesses | where-object {$_.ParentProcessId -eq $Process.ProcessId -and $_.CreationDate -ge $Process.CreationDate}
+        if ($null -ne $Children) {
+            foreach ($Child in $Children) {
+                Get-ProcessAndChildProcesses ($Level + 1) $Child
+            }
+        }
+    }
+
+    $AllProcesses = Get-CimInstance -ClassName "win32_process"
+    $RootProcesses = @()
+    # Process "System Idle Process" is processed differently, as ProcessId and ParentProcessId are 0
+    # $AllProcesses is sliced from index 1 to the end of the array
+    foreach ($Process in $AllProcesses[1..($AllProcesses.length-1)]) {
+        $Parent = $AllProcesses | where-object {$_.ProcessId -eq $Process.ParentProcessId -and $_.CreationDate -lt $Process.CreationDate}
+        if ($null -eq $Parent) {
+            $RootProcesses += $Process
+        }
+    }
+    # Process the "System Idle process" separately
+    "[{0,-5}] [{1}]" -f $AllProcesses[0].ProcessId, $AllProcesses[0].Name
+    foreach ($Process in $RootProcesses) {
+        Get-ProcessAndChildProcesses 0 $Process
+    }
+}
+Write-Host "defined Print-ProcessTree" -ForegroundColor DarkGreen
+
 function global:Update-This-Profile()
 {
     <#
