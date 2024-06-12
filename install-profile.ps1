a1f74bfc545cfbdb49d0134485b498a9b76e8972
James Thomas Moon <815261+jtmoon79@users.noreply.github.com>
2024-06-11 19:50:58 -0700

    install-profile.ps1 note Invoke-Expression expression

--- install-profile.ps1
+++ install-profile.ps1
@@ -5,6 +5,11 @@
 # Run this script remotely:
 #
 #    $(Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jtmoon79/dotfiles/master/install-profile.ps1").Content | powershell -NoLogo -NoProfile -NonInteractive -Command -
+#
+# Invoke this profile without copying to $PROFILE
+#
+#    Invoke-Expression $(Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jtmoon79/dotfiles/master/Microsoft.PowerShell_profile.ps1").Content
+#
 
 $ProfileInfo = [System.IO.FileInfo]$PROFILE
 if (-not (Test-Path $ProfileInfo.DirectoryName)) {
