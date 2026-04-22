8a84fcbd725f7797566dcbe5a786aaa512ac79e0
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2026-04-21 20:34:54 -0700

    docker-tests.sh add bash 5.3, chmod +x

--- docker-tests.sh
+++ docker-tests.sh
@@ -19,20 +19,25 @@ CONT_TMP=${HOST_TMP}
 
 # images at https://hub.docker.com/_/bash
 images=(
-    'bash:3.2'
-    'bash:4.0'
-    'bash:4.1'
-    'bash:4.2'
-    'bash:4.3'
-    'bash:4.4'
-    'bash:5.0'
+    'docker.io/library/bash:3.2'
+    'docker.io/library/bash:4.0'
+    'docker.io/library/bash:4.1'
+    'docker.io/library/bash:4.2'
+    'docker.io/library/bash:4.3'
+    'docker.io/library/bash:4.4'
+    'docker.io/library/bash:5.0'
+    'docker.io/library/bash:5.3'
 )
 
 for image in "${images[@]}"; do
-    test_pass=${HOST_TMP}/${image//:/_}
+    name=${image//:/_}
+    name=${name//\//_}
+    test_pass=${HOST_TMP}/${name}
+    echo -e "\e[93mImage ${image} will create file ${test_pass}\e[39m" >&2
     (
     set -x
     docker run \
+               --pull=missing \
                -e '__FORCE_INTERACTIVE=true' \
                -e 'color_force=true' \
                -v "${HOST_TMP}:${CONT_TMP}" \
@@ -40,8 +45,8 @@ for image in "${images[@]}"; do
                --rm \
                -it "${image}" \
                sh -c "
-        set -x;
-        echo 'echo \"PS1:\"; echo \"\${PS1}\"; eval echo \"\\\"\${PS1}\\\"\"; touch \"${test_pass}\"; exit' | bash --login;
+        set -eux;
+        echo 'set -eu; echo \"PS1:\"; echo \"\${PS1}\"; eval echo \"\\\"\${PS1}\\\"\"; touch \"${test_pass}\"; exit' | bash --login;
 "
     )
     echo >&2
